ef2c06b664d9a19efbb40eb75116fe88d106250c
muhnuryusri <naoruyuzuri@gmail.com>
2021-09-09 19:04:49 +0800

    dev(todo-us1-1): add login user

--- app/src/main/java/com/application/todolistglints/ui/authentication/login/LoginViewModel.kt
+++ app/src/main/java/com/application/todolistglints/ui/authentication/login/LoginViewModel.kt
@@ -1,4 +1,18 @@
 package com.application.todolistglints.ui.authentication.login
 
-class LoginViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.application.todolistglints.data.UserEntity
+import com.application.todolistglints.utils.DataDummy
+
+class LoginViewModel : ViewModel() {
+
+    fun getUsername(username: String, password: String): Boolean {
+        for(item in DataDummy.generateDummyUser()) {
+            if (username == item.username && password == item.password) {
+                return true
+            }
+        }
+        return false
+    }
 }
